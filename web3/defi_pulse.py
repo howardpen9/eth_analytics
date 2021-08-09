cfa8c4d814c75d9af9d6b449bf40efad28f29ba5
Howard Peng <howard.pen9@gmail.com>
2021-08-09 12:43:17 +0800

    Howard Test
    Test
    
    Co-Authored-By: Luke Posey <33185528+Poseyy@users.noreply.github.com>

--- web3/defi_pulse.py
+++ web3/defi_pulse.py
@@ -1,12 +1,23 @@
-import requests 
-import json 
-
+import requests
+import json
+import pandas as pd
 
 # input your token here
-api_key = ''
+api_key = "73e72770e1757bca83127c49ed61bb42fc04f42b0f4e6b310131aa1f2b0e"
 
-url = 'https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?api-key=' + api_key
+url = (
+    "https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?api-key=" + api_key
+)
 response = requests.get(url)
 result = json.loads(response.text)
-print(result[0]['tvlUSD'])
 
+
+def get_pretty_json_string(value):
+    return json.dumps(value, indent=4, sort_keys=True, ensure_ascii=False)
+
+
+## print(get_pretty_json_string(result))
+## print(result)
+
+print(pd.json_normalize(result))
+print(result[0]["tvlUSD"])
